2b6d2f2f9f41b721e63af728bc35cbebafe88fcc
kadey001 <kadey001@ucr.edu>
2025-08-21 02:34:55 -0700

    updated drone icons, increased marker size, select random icon for markers

--- src/utils/marker.ts
+++ src/utils/marker.ts
@@ -1,13 +1,19 @@
 import { getUAVColor } from './color';
-import DroneIcon from '../components/icons/DroneIconB.svg?raw';
+import DroneIconA from '../components/icons/DroneIconA.svg?raw';
+import DroneIconB from '../components/icons/DroneIconB.svg?raw';
+import DroneIconC from '../components/icons/DroneIconC.svg?raw';
 import type { UAV } from '@/types';
 
 export const createMarkerElement = (uav: UAV) => {
     const marker = document.createElement('div');
-    marker.innerHTML = DroneIcon;
+    const getRandomIcon = () => {
+        const icons = [DroneIconA, DroneIconB, DroneIconC];
+        return icons[Math.floor(Math.random() * icons.length)];
+    };
+    marker.innerHTML = getRandomIcon();
     const svg = marker.querySelector('svg');
     if (svg) {
-        svg.classList.add('w-8', 'h-8', 'drop-shadow-[0_2px_2px_rgba(0,0,0,0.7)]');
+        svg.classList.add('w-10', 'h-10', 'drop-shadow-[0_2px_2px_rgba(0,0,0,0.7)]');
         svg.setAttribute('fill', getUAVColor(uav));
     }
     return marker;
